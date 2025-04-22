e14cd1edf02471237e8f812230d3d4aff612b0f4
Lê Thành Nghĩa <lethanhnghia0938@gmail.com>
2025-04-22 16:03:57 +0700

    kết nối database

---
+++ src/main/java/DB/DBConnection.java
@@ -0,0 +1,23 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Thay user/pass nếu bạn có cài khác
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/giasutot?useSSL=false&serverTimezone=UTC",
+                    "root",
+                    ""
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
+
